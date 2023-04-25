af2d88c9541ac81f996dabf3338f5ddc3da182ae
Xcaliburns <davidabruzzogames@gmail.com>
2023-04-25 13:09:49 +0200

    lat mapped review services mapping

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,24 @@ import "./App.css";
 
 function App() {
   const [services, setServices] = useState([]);
+  const[lat, setLat] = useState([]);
+  const[lon, setLon] = useState([]);
+  const[prixDiesel, setPrixDiesel] = useState(0);
+  
   // const apiKey = import.meta.env.VITE_API_KEY;
-  const baseURL = `https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-carburants-en-france-flux-instantane-v2&q=&facet=carburants_disponibles&facet=carburants_indisponibles&facet=services_service&&rows=10`;
+  const baseURL = `https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-carburants-en-france-flux-instantane-v2&q=&facet=carburants_disponibles&facet=carburants_indisponibles&facet=services_service&facet=latitude&&rows=10`;
 
   const handleSearch = (e) => {
     e.preventDefault();
     axios.get(baseURL).then((response) => {
-      setServices(Object.values(Object.values(response.data.facet_groups)[2])[1]);
-     console.log(response.data.facet_groups[2]);
+      setServices(
+        Object.values(Object.values(response.data.facet_groups)[2])[1]
+      );
+      setLat(
+      Object.values(Object.values(Object.values(response.data.facet_groups[3].facets)))
+      );
+      console.log(response.data.facet_groups[2]);
+      console.log(Object.values(Object.values(response.data.facet_groups[3].facets)));
     });
   };
 
@@ -33,9 +43,17 @@ function App() {
             onClick={handleSearch}
           />
         </form>
-      
       </header>
-     <ul >{services.slice(1).map((service)=>(<li key={name}>  {service.name}</li>)) }</ul> 
+      <ul>
+        {services.map((service) => (
+          <li key={name}> {service.name}</li>
+        ))}
+      </ul>
+      <ul>
+        {lat.map((lat) => (
+          <li key={name}> {lat.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
