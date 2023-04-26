ef3df4552ccf157fc65a37e7abbb721bae8a1693
Xcaliburns <davidabruzzogames@gmail.com>
2023-04-26 14:56:43 +0200

    probleme avec les rows de la requete trop grands

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,45 @@
-import { useState } from "react";
-import axios from "axios";
-
+// import { useState } from "react";
+// import axios from "axios";
+import Search from "./components/Search";
 import "./App.css";
 
 function App() {
-  const [services, setServices] = useState([]);
-  const [facets, setFacets] = useState([]);
-  const [records, setRecords] = useState([]);
-  const [lat, setLat] = useState("");
-  const [lon, setLon] = useState("");
-  const [prixDiesel, setPrixDiesel] = useState(0);
+  // const [records, setRecords] = useState([]);
 
-  
-  const baseURL = `https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-carburants-en-france-flux-instantane-v2&q=&facet=carburants_disponibles&facet=carburants_indisponibles&facet=services&facet=latitude&&rows=10`;
+  //  const baseURL = `https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-carburants-en-france-flux-instantane-v2&&&rows=10`;
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    axios.get(baseURL).then((response) => {
-      setRecords(response.data.records);  
+  // const handleSearch = (e) => {
+  //   e.preventDefault();
+  //   axios.get(baseURL).then((response) => {
+  //     setRecords(response.data.records);
+  //   });
+  // };
 
-      
-    });
-  };
-
-  
   return (
     <>
       <header className="toto">
-        <form className="tutu">
-     
+        <Search />
+        {/* <form className="tutu">
           <input
             className="tutu"
             type="submit"
             value="Search"
             onClick={handleSearch}
           />
-        </form>
+        </form> */}
       </header>
 
-      <table></table>
-      <ul>
+      {/* <ul>
         {records.map((record) => (
           <li key="recordid">
             {" "}
-            <li>  département : {record.fields.departement}   </li>
-            <li> code postal :{record.fields.cp} </li>
-            <li>coordonnées :lon:{record.fields.geom[0]} lat:{record.fields.geom[1]}</li>
-            {console.log(record.fields.prix)}
-              <li> prix des carburants
-                {JSON.parse(record.fields.prix).map((prix_carburant) => (
+            <li>
+              {record.fields.adresse} {record.fields.cp} {record.fields.ville}
+            </li>
+            <li>
+              {" "}
+              prix des carburants
+              {JSON.parse(record.fields.prix).map((prix_carburant) => (
                 <ul key={prix_carburant["@id"]}>
                   <li> {prix_carburant["@nom"]}</li>
                   <li>Prix: {prix_carburant["@valeur"]} €</li>
@@ -57,7 +48,7 @@ function App() {
             </li>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+function Search() {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("    ");
+
+  const baseURL = `https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-carburants-en-france-flux-instantane-v2&&&rows=9987`
+
+ useEffect(() => {
+  axios.get(baseURL)
+    .then((response) => {
+      setResults(response.data.records);
+    })
+    .catch((error) => {
+      console.error('Une erreur est survenue :', error);
+    });
+}, []);
+ 
+
+  const handleChange = (e) => {   
+  let value = e.target.value;
+    value.length >2 &&  setQuery(value);
+  };
+console.log(query);
+  return (
+    <>
+     
+      <div className="search">
+        <input
+          type="text"
+          name="searchBar"
+          id="searchBar"
+          placeholder="rechercher"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="search_results">
+        {results
+          .filter((val) => {
+            return val.fields.ville.toLowerCase().includes(query.toLocaleLowerCase());
+          })
+          .map((val) => {
+            return (
+              <div className="search_result" key={val.recordid}>
+                {console.log(val.fields.ville)}
+                {val.fields.ville} 
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+}
+
+export default Search;
